716ffefad4e800425326fabf8fa3b13ccdcf5ed2
m-fiks <fiksd005@umn.edu>
2018-05-04 16:47:54 -0500

    prompt functions working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,36 +9,64 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'What is the ID of the product?',
-        name: 'prodID'
-    }
-]).then((answers)=>{
-    console.log(answers.prodID);
-    let id = answers.prodID;
-
-    connection.connect((err, data)=>{
-        if (err) throw err;
-        //console.log('CONNECTED');
-        //? is placeholder for id variable
-        let sqlVar = "SELECT * FROM products WHERE id = ?";
-        connection.query(sqlVar, [id], (err, data) => {
-            if(err) throw err;
-            data.forEach((elem)=>{
-                let id = elem.id;
-                let name = elem.product_name;
-                let price = elem.price;
-                let quantity = elem.stock_quantity;
-                console.log(`${id}.) ${name}, $${price}`);
-                console.log(quantity)
+function getID() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the ID of the product?',
+            name: 'prodID'
+        }
+    ]).then((answers)=>{
+        //console.log(answers.prodID);
+        if (isNaN(answers.prodID) === false){
+            let id = answers.prodID;
+            connection.connect((err, data)=>{
+                if (err) throw err;
+                //console.log('CONNECTED');
+                
+                //? is placeholder for id variable, pass id as 2nd param
+                let sqlVar = "SELECT * FROM products WHERE id = ?";
+                connection.query(sqlVar, [id], (err, data) => {
+                    if(err) throw err;
+                    data.forEach((elem)=>{
+                        let sqlID = elem.id;
+                        let name = elem.product_name;
+                        let price = elem.price;
+                        let quantity = elem.stock_quantity;
+                        console.log(`${name} in stock at a price of:$${price}. Quantity: ${quantity}.`);
+                        userNeeds();
+                    });
+                })
+                //end connection
+                connection.end();
             })
-        })
-        //end connection
-        connection.end();
+        }
+        else{
+            console.log('Please enter in a valid product ID (number 1-11)');
+            getID();
+        }
+    });
+};
+
+getID();
+
+function userNeeds () {
+    inquirer.prompt([
+        {
+        type: 'input',
+        message: 'How many would you like?',
+        name: 'userQuantity'
+        }
+    ]).then((answers)=>{
+        if (isNaN(answers.userQuantity) === false){
+
+        }
+        else{
+            console.log(`Please enter in a valid desired quantity.`)
+            userNeeds();
+        }
     })
-});
+}
 
 
 
