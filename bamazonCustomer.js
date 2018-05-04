a9a40dd5f5dcd0c5c39e12628738270cba2fa41a
m-fiks <fiksd005@umn.edu>
2018-05-04 10:47:00 -0500

    beginning prompt func

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,21 +9,47 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect((err, data)=>{
-    if (err) throw err;
-    //console.log('CONNECTED');
-    let sqlVar = "SELECT * FROM products";
-    connection.query(sqlVar, (err, data) =>{
-        if(err) throw err;
-        data.forEach((elem)=>{
-            let id = elem.id;
-            let name = elem.product_name;
-            let price = elem.price;
-            let quantity = elem.stock_quantity;
-            console.log(`${id}.) ${name}, $${price}`);
-            console.log(quantity)
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the ID of the product?',
+        name: 'prodID'
+    }
+]).then((answers)=>{
+    console.log(answers.prodID);
+    let id = answers.prodID;
+
+    connection.connect((err, data)=>{
+        if (err) throw err;
+        //console.log('CONNECTED');
+        //? is placeholder for id variable
+        let sqlVar = "SELECT * FROM products WHERE id = ?";
+        connection.query(sqlVar, [id], (err, data) => {
+            if(err) throw err;
+            data.forEach((elem)=>{
+                let id = elem.id;
+                let name = elem.product_name;
+                let price = elem.price;
+                let quantity = elem.stock_quantity;
+                console.log(`${id}.) ${name}, $${price}`);
+                console.log(quantity)
+            })
         })
+        //end connection
+        connection.end();
     })
-    //end connection
-    connection.end();
-})
+});
+
+
+
+
+// .then((answers) => {
+//     console.log(answers.artist)
+//     let sql = "SELECT * FROM top5000 WHERE ?";
+//     connection.query(sql, {artist: answers.artist}, (err, result) => {
+//         if (err) throw err;
+//         for(key in result){
+//             console.log(result[key].title);
+//         }
+//     })
+// })
