f95ac4271b33ff7bf9425a54eaf863fb755ae7bf
m-fiks <fiksd005@umn.edu>
2018-05-05 20:21:20 -0500

    future ideas of showing items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,6 +10,20 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+// const items = {
+//     '1' : 'Purina One Wet Cat Food',
+//     '2' : 'Downy Wrinkle Release Spray',
+//     '3' : 'Essie Nail Polish, white',
+//     '4' : 'Manic Panic Hair Bleach',
+//     '5' : 'Black Diamond Momentum Harness',
+//     '6' : 'Wear24 Android Wear 2.0 Watch',
+//     '7' : 'Star Trek: The Next Generation Season 5',
+//     '8' : 'iRobot Roomba',
+//     '9' : 'Tassel Earrings',
+//     '10' : 'Hankook Off-Road-Tire',
+//     '11' : 'Goodnight Moon'
+// }
+
 //display data from mySQL for item
 function displayData (data) {
     data.forEach((elem)=>{
@@ -19,31 +33,31 @@ function displayData (data) {
         let quantity = elem.stock_quantity;
         console.log(clc.cyan(`${name} in stock at a price of:$${price}. Quantity: ${quantity}.`));
         userNeeds(quantity,price,sqlID);
-
     })
 };
 
+//first, show products then:
 //prompt user - get ID for product
 function getID() {
     inquirer.prompt([{
         type: 'input',
-        message: 'What is the ID of the product?',
+        message: 'What is the ID of the product you wish to purchase?',
         name: 'prodID'
     }]).then((answers)=>{
         //console.log(answers.prodID);
         if (isNaN(answers.prodID) === false){
             let id = answers.prodID;
+            
+            //connect to DB
             connection.connect((err, data)=> {
                 if (err) throw err;
-                //console.log('CONNECTED');
-                
                 //? is placeholder for id variable, pass id as 2nd param
                 let sqlVar = "SELECT * FROM products WHERE id = ?";
                 connection.query(sqlVar, [id], (err, data) => {
                     if(err) throw err;
                     displayData(data);
                 })
-            })
+            });
         }
         else{
             console.log(clc.red.bold('Please enter in a valid product ID (number 1-11)'));
@@ -62,14 +76,14 @@ function userNeeds (quantity,price,id) {
         name: 'userQuantity'
         }
     ]).then((answers)=>{
-        console.log(id);
+        //console.log(id);
         if (isNaN(answers.userQuantity) === false){
             let userQuantity = answers.userQuantity;
             if (userQuantity < quantity && userQuantity > 0){
                 //update db to quantity - userQuantity
                 let newQuant = quantity - userQuantity;
                 // console.log(newQuant)
-                
+
                 let sqlUpdate = "UPDATE products SET stock_quantity = ? WHERE id = ?"
                 connection.query(sqlUpdate, [newQuant,id], (err, data) => {
                     if(err) throw err;
@@ -78,6 +92,8 @@ function userNeeds (quantity,price,id) {
                 //multiple userQuantity * price to display total
                 let total = (userQuantity * price).toFixed(2);
                 console.log(`Your total is $${total}.`)
+                //end connection
+                connection.end();
             }
             else if (userQuantity == 0) {
                 console.log(clc.red.bold(`Please enter in a valid desired quantity.`))
@@ -92,10 +108,6 @@ function userNeeds (quantity,price,id) {
             console.log(clc.red.bold(`Please enter in a valid desired quantity.`))
             userNeeds(quantity,price,id);
         }
-
     })
-    
 }
 
-        //end connection
-        // connection.end();
